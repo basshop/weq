8fbb95ff75a2ec266e4ae776c94a9299eb669651
basshop <755jkm@gmail.com>
2025-09-25 12:47:35 +0700

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,11 @@
-// // lib/db.ts
-// import mysql from "mysql2/promise"
-
-// export const db = mysql.createPool({
-//   host: process.env.MYSQL_HOST || "localhost",
-//   user: process.env.MYSQL_USER || "xdowxyz_lul_",
-//   password: process.env.MYSQL_PASSWORD || "3DM*xp0lm*atbn3V",
-//   database: process.env.MYSQL_DATABASE || "xdowxyz_lul_",
-// })
-// lib/db.ts
-// lib/db.ts
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: process.env.MYSQL_HOST || "103.30.127.7",   // เซิร์ฟเวอร์ MySQL
-  user: process.env.MYSQL_USER || "betashif_py2",        // User MySQL
-  password: process.env.MYSQL_PASSWORD || "pdXXFwrDDJgHwWV6AXmA",    // รหัสผ่าน
-  database: process.env.MYSQL_DATABASE || "betashif_py2" // ชื่อฐานข้อมูล
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-
